371cf87e8ea1711816913ef2716ea7f2d6fb1249
Mohamad Mahdi <m_pashapour@comp.iust.ac.ir>
2026-09-22 19:39:26 -0700

    create two post by admin panel
    work with admin panel and create 2 posts and register post on admin and formatin the name of post for display in admin page

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,6 @@ class Post(models.Model):
     createded_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now = True)
     published_date = models.DateTimeField(null = True)
+
+    def __str__(self):
+        return "{} - {}".format(self.title, self.id)
