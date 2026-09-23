3ba46340e8b4f9925fe3d366f59c9d4ac540b229
Mohamad Mahdi <m_pashapour@comp.iust.ac.ir>
2026-09-23 00:03:46 -0700

    filter the posts thar published time <= now && increment the counted_view
    by every view the any post the number of counter view will be increase . and only the posts will be displaied their published_time <= now

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render , get_object_or_404
 from blog.models import Post
+from django.utils import timezone
+from django.db.models import F
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now()
+    )
     context  = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)
 
@@ -21,5 +26,12 @@ def test(request , pid):
 
     # post = Post.objects.get(id = pid)
     post = get_object_or_404(Post , pk=pid)
+
+    # increment the counted_view
+    post.counted_view = F('counted_view') + 1
+    post.save(update_fields=['counted_view'])
+
+    # give the current counter from the DB after update
+    post.refresh_from_db()
     context = {'post' : post}
     return render(request , 'test.html' , context)
