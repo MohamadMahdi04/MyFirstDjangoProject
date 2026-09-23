b183825b13806cba381238d95a6ba95ee6e3e21d
Mohamad Mahdi <m_pashapour@comp.iust.ac.ir>
2026-09-22 23:42:04 -0700

    urls by posts id and use get_object_or_404 method
    in this part
    i had work on the blog urls for the posts by their id
    and use get_object_or_404 for that if the post not exist , redirct to the 404 page

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path ( " url address" , ' view ' , name)
     path("" , blog_view , name='index'),
     path("single" , blog_single , name='single'),
-    path("test" , test , name='test')
+    # path("<str:name>/<str:family_name>/<int:age>" , test , name='test')
+    path("post-<int:pid>", test , name='test')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from blog.models import Post
 # Create your views here.
 def blog_view(request):
@@ -10,8 +10,16 @@ def blog_single(request):
     context = {'title' : 'BitCoin has been Fucked !' , 'content' : 'BitCoin price is now 000!' , 'author' : 'Mahdi Pashapur'}
     return render(request , 'blog/blog-single.html' , context)
 
-def test(request):
+# def test(request , name , family_name , age):
+def test(request , pid):
     # posts = Post.objects.filter(status=1)
-    posts = Post.objects.all()
-    context  = {'posts' : posts}
+    # posts = Post.objects.all()
+    # context  = {'posts' : posts}
+
+    # context = {'name' : name , 'family_name' : family_name , 'age' : age}
+    # context = {'pid' : pid}
+
+    # post = Post.objects.get(id = pid)
+    post = get_object_or_404(Post , pk=pid)
+    context = {'post' : post}
     return render(request , 'test.html' , context)
