e7e08bc2895d39497deac6c46c550bb7f356449e
Mohamad Mahdi <m_pashapour@comp.iust.ac.ir>
2026-09-22 22:59:21 -0700

    implement test page for posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
     return render(request , 'blog/blog-home.html')
@@ -7,3 +7,9 @@ def blog_view(request):
 def blog_single(request):
     context = {'title' : 'BitCoin has been Fucked !' , 'content' : 'BitCoin price is now 000!' , 'author' : 'Mahdi Pashapur'}
     return render(request , 'blog/blog-single.html' , context)
+
+def test(request):
+    # posts = Post.objects.filter(status=1)
+    posts = Post.objects.all()
+    context  = {'posts' : posts}
+    return render(request , 'test.html' , context)
