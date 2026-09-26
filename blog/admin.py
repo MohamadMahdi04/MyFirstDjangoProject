056cced22784d0f501f12aac3bd8a64feaa7b0e0
Mohamad Mahdi <m_pashapour@comp.iust.ac.ir>
2026-09-25 23:32:18 -0700

    add author field in post

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'createded_date'
     empty_value_display = '-empty-'
-    list_display = ('title' , 'counted_view' , 'status' , 'published_date' , 'createded_date' ,)
-    list_filter = ['status' ]
+    list_display = ('title' , 'counted_view' , 'author' , 'status' , 'published_date' , 'createded_date' ,)
+    list_filter = ['status' , 'author']
     ordering = ['-createded_date' ]
     search_fields = ['title' , 'content']
 admin.site.register(Post , PostAdmin)
